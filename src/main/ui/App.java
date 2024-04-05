e1067bf947fab125e93c19a91e27584077d1219d
UBC Student <chancelp@students.cs.ubc.ca>
2024-04-05 15:49:09 -0700

    Whoops

--- src/main/ui/App.java
+++ src/main/ui/App.java
@@ -106,7 +106,7 @@ public class App {
             maxBoundPlayedList();
             finishedSong();
         } else {
-            //Gui.songFinishedPlaying();
+            Gui.songFinishedPlaying();
         }
     }
 
