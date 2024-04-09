aefedc6ff22bc650f1d3390db4015bcfc37db103
UBC Student <chancelp@students.cs.ubc.ca>
2024-04-09 16:19:11 -0700

    Fixed a bug with tag setting (idk how I missed this all this time XD)

--- src/main/audio/filetypes/decoders/Flac.java
+++ src/main/audio/filetypes/decoders/Flac.java
@@ -257,8 +257,11 @@ public class Flac implements AudioDecoder {
             Object data = container.getID3Data(entry.getKey());
             if (data != null) {
                 ExceptionIgnore.ignoreExc(() -> tag.setField(entry.getValue(), data.toString()));
+            } else if (tag.hasField(entry.getValue())) {
+                ExceptionIgnore.ignoreExc(() -> tag.deleteField(entry.getValue()));
             }
         }
+        f.setTag(tag);
         ExceptionIgnore.ignoreExc(() -> f.commit());
     }
 
