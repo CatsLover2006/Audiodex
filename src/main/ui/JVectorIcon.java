2a97db8bcec7fb2fa86d2823e6b8da9b0fa51352
UBC Student <chancelp@students.cs.ubc.ca>
2024-04-09 18:50:27 -0700

    Better segmentation Also vectors are rendered when they're needed, not in advance

--- src/main/ui/JVectorIcon.java
+++ src/main/ui/JVectorIcon.java
@@ -12,73 +12,24 @@ import static java.util.Arrays.asList;
 
 // Vector Icon class
 public class JVectorIcon extends ImageIcon {
-    private SVGDocument svg;
-    private int width;
+    private JVectorImageColorable image;
     private int height;
-    
-    // Effects: gets window scale
-    public static double getWindowScale(Window window) {
-        GraphicsDevice device = getWindowDevice(window);
-        return device.getDisplayMode().getWidth() / (double) device.getDefaultConfiguration().getBounds().width;
-    }
-    
-    // Effects: gets graphics device attached to this window
-    public static GraphicsDevice getWindowDevice(Window window) {
-        Rectangle bounds = window.getBounds();
-        return asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()).stream()
-                // pick devices where window located
-                .filter(d -> d.getDefaultConfiguration().getBounds().intersects(bounds))
-                // sort by biggest intersection square
-                .sorted((f, s) -> Long.compare(//
-                        square(f.getDefaultConfiguration().getBounds().intersection(bounds)),
-                        square(s.getDefaultConfiguration().getBounds().intersection(bounds))))
-                // use one with the biggest part of the window
-                .reduce((f, s) -> s) //
-                // fallback to default device
-                .orElse(window.getGraphicsConfiguration().getDevice());
-    }
-    
-    // Effects: returns area of Rectangle
-    public static long square(Rectangle rec) {
-        return Math.abs(rec.width * rec.height);
-    }
+    private int width;
     
 
     // Effects: "registers" the SVGDocument
     public JVectorIcon(SVGDocument document, int w, int h) {
-        svg = document;
+        image = new JVectorImageColorable(document, w, h);
+        super.setImage(image);
         width = w;
         height = h;
-        setImage(new BaseMultiResolutionImage(renderScaledImage(new Color(0), 1.0),
-                renderScaledImage(new Color(0), 2.0), renderScaledImage(new Color(0), 3.0)));
     }
 
     // Effects: Paints icon (in foreground color)
     @Override
     public void paintIcon(Component c, Graphics graphics, int x, int y) {
-        setImage(new BaseMultiResolutionImage(renderScaledImage(c.getForeground(), 1.0),
-                renderScaledImage(c.getForeground(), 2.0), renderScaledImage(c.getForeground(), 3.0)));
+        image.setColor(c.getForeground());
         super.paintIcon(c, graphics, (c.getWidth() - width) / 2, (c.getHeight() - height) / 2);
-    }
-    
-    // Effects: renders icon at resolution
-    private BufferedImage renderScaledImage(Color fgColor, double scale) {
-        BufferedImage render = new BufferedImage((int)(width * scale), (int) (height * scale),
-                BufferedImage.TYPE_INT_ARGB);
-        Graphics2D draw = render.createGraphics();
-        draw.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        draw.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-        svg.render(null, draw, new ViewBox((int)(width * scale), (int) (height * scale)));
-        draw.dispose();
-        for (int workX = 0; workX < (int)(width * scale); workX++) {
-            for (int workY = 0; workY < (int)(height * scale); workY++) {
-                Color pixel = new Color(render.getRGB(workX, workY), true);
-                int rgba = (pixel.getAlpha() << 24) | (fgColor.getRed() << 16)
-                        | (fgColor.getGreen() << 8) | fgColor.getBlue();
-                render.setRGB(workX, workY, rgba);
-            }
-        }
-        return render;
     }
 
     // Effects: returns width
